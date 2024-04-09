9a414d2a40fe6e0abcd7df8cfd2c24846237e70a
adsk-dev <73867345+adskoe96@users.noreply.github.com>
2024-04-09 12:52:28 +0300

    Create DirectionalSpriteBillboard.cs
    added file

---
+++ DirectionalSpriteBillboard.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+internal class DirectionalSpriteBillboard : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform parent;
+    private static Vector3 cameraForward, parentForward;
+
+    private void Update()
+    {
+        try { cameraForward = Camera.main.transform.forward; }
+        catch { return; }
+        
+        parentForward = parent.forward;
+        float angleBetween = Vector3.SignedAngle(parentForward, cameraForward, Vector3.up);
+
+        float normalizedAngle = (angleBetween + 360) % 360;
+
+        if (normalizedAngle > 180)
+        {
+            normalizedAngle -= 360;
+        }
+
+        animator.SetFloat("x", Mathf.Sin(normalizedAngle * Mathf.Deg2Rad));
+        animator.SetFloat("z", Mathf.Cos(normalizedAngle * Mathf.Deg2Rad));
+    }
+}
